66ecdd8766fe8383a0014c10962643f374ddac51
jlee7362 <jlee736222@gmail.com>
2025-07-09 20:51:36 +0900

    servlet 시작

---
+++ src/main/java/koreaIT/HomeManinServlet.java
@@ -0,0 +1,20 @@
+package koreaIT;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HomeManinServlet")
+public class HomeManinServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		response.getWriter().append("Served at: 한글 hellllooooo").append(request.getContextPath());
+	}
+
+}
