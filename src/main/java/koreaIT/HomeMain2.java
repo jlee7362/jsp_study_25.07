2e633d53662b4f5afbaa57e73dd03416e653cb1d
jlee7362 <jlee736222@gmail.com>
2025-07-09 21:42:31 +0900

    servlet 구구단

--- src/main/java/koreaIT/HomeMain2.java
+++ src/main/java/koreaIT/HomeMain2.java
@@ -8,13 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/HomeManinServlet") //URL 주소
-public class HomeManinServlet extends HttpServlet {
+@WebServlet("/HomeMain2")
+public class HomeMain2 extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
-		response.getWriter().append("Served at: 한글 hellllooooo").append(request.getContextPath());
-		response.getWriter().append("<br><a href=\"http://localhost:8080/jlee_servlet/HomeMain2\" target=\"_blank\">main2로 가기</a>");
+		response.getWriter().append("Served at:hello ").append(request.getContextPath());
+		response.getWriter().append("<br><a href=\"http://localhost:8080/jlee_servlet/HomeManinServlet\" target=\"_blank\">Home으로 가기</a>");
 	}
+
 }
