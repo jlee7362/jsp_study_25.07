ca0f5fb1f935b59459650fa8493d7b580e0eeb96
jlee7362 <jlee736222@gmail.com>
2025-07-15 19:55:29 +0900

    printDan.jsp 파일 추가로 더 편해진 html&css

--- src/main/java/koreaIT/PrintDan.java
+++ src/main/java/koreaIT/PrintDan.java
@@ -11,30 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 public class PrintDan extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");
-		
-		String inputDan = request.getParameter("dan");
-		String inputLimit = request.getParameter("limit");
-		String color =request.getParameter("color");
-		System.out.println(inputDan);
-		System.out.println(inputLimit);
-		
-		if (inputDan == null) {
-			inputDan = "2";
-		}
-		if (inputLimit == null) {
-			inputLimit = "1";
-		}
-		int dan = Integer.parseInt(inputDan);
-		int limit = Integer.parseInt(inputLimit);
-		
-		response.getWriter().append(String.format("<style> div {color: %s;} h1{color: blue;}</style>", color));
-		
-		response.getWriter().append(String.format("<h1> == %d 단 ==</h1>", dan));
-		
-		for(int i = 1; i <= limit; i++) {
-			response.getWriter().append(String.format("<div>%d * %d = %d</div>", dan, i, (dan*i)));
-		}
+		request.getRequestDispatcher("/jsp/home/printDan.jsp").forward(request, response);
 	}
-
 }
